73cee9d379853ac66e95665e01d1fdfa8dcb05ce
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-24 10:55:13 -0400

    added servers script

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,7 +2,7 @@
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
-for i in "$(NAMES[@])"
+for i in "${NAMES[@]}"
 do
     echo "NAME: $i"
 done
