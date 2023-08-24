f5df5a09a0e3e56a8d106199722830828d14dfa8
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-08-24 12:12:11 -0400

    added servers script

--- create-ec2.sh
+++ create-ec2.sh
@@ -9,13 +9,13 @@ SECURITY_GROUP_ID=sg-011b98c08b45f2c5d
 
 for i in "${NAMES[@]}"
 do
-    if [[ $i == "mongodb" || $i == "mysql"]]
+    if [[ $i == "mongodb" || $i == "mysql" ]]
     then 
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "creating $i instance: $IP_ADDRESS"
 done
